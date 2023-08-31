5ad94e4c724d77c70c1148075e8641a591d4b674
adibwp <adwirawp@gmail.com>
2023-08-31 12:28:51 +0700

    Add domain layer

---
+++ lib/src/features/movie_list/domain/usecase/get_movie_list.dart
@@ -0,0 +1,12 @@
+import '../entities/movie.dart';
+import '../repositories/movie_repository.dart';
+
+class GetMovieListUseCase {
+  final MovieRepository movieRepository;
+
+  GetMovieListUseCase(this.movieRepository);
+
+  Future<MovieEntity> getMovieList() {
+    return movieRepository.getMovieList();
+  }
+}
